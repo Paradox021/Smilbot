80999fd3002b8714412a7578062ec780e826de4c
Paradox021 <espanacarmelo@gmail.com>
2022-10-15 17:15:11 +0200

    Smilbot v1.0

---
+++ Smilbot.js
@@ -0,0 +1,50 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js')
+const fetch = require("node-fetch");
+const prefix = '!'
+
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
+
+let auth = require('./auth.json');
+
+client.on("ready", () => {
+    console.log(`bot is online as ${client.user.tag}!`)
+
+    client.user.setActivity(`busy being Smilbot!!`)
+})
+
+client.on("messageCreate", (message) => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return
+
+
+    const args = message.content.slice(prefix.length).split(/ +/)
+
+    const command = args.shift().toLowerCase()
+
+   
+    
+    if(command === 'ping') message.channel.send('pong!')
+
+    if(command === 'dados') message.channel.send(lanzaDados())
+
+    if(command === 'quote') inspirationalQuote().then(quote => message.channel.send(quote.text + " -"+quote.author))
+    
+    if(command === 'cat') getCat().then(imageUrl => message.channel.send({files:[imageUrl]}))
+
+})
+client.login(auth.token)
+
+function lanzaDados(){
+    return (Math.floor(Math.random()*101)).toString()
+}
+
+function inspirationalQuote(){
+    return fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(data => data[Math.floor(Math.random() * 1500)])
+}
+
+function getCat(){
+    return fetch("https://api.thecatapi.com/v1/images/search")
+    .then(response => response.json())
+    .then(data => data[0].url)
+}
