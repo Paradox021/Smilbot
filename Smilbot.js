35081e7c0af830b96834a98ddd8dd410e27cf659
Paradox021 <espanacarmelo@gmail.com>
2022-10-29 22:59:31 +0200

    play and stop commands

--- Smilbot.js
+++ Smilbot.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js')
 const fetch = require("node-fetch");
-const prefix = '!'
+const prefix = '.'
+const { DisTube } = require('distube')
 
 const client = new Client({
     intents: [
@@ -10,6 +11,13 @@ const client = new Client({
         GatewayIntentBits.GuildVoiceStates
     ]
 })
+
+client.distube = new DisTube(client, {
+    leaveOnStop: true,
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    emitAddListWhenCreatingQueue: false
+  })
 
 let auth = require('./auth.json');
 
@@ -22,23 +30,42 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
     if(!message.content.startsWith(prefix) || message.author.bot) return
 
-
     const args = message.content.slice(prefix.length).split(/ +/)
 
     const command = args.shift().toLowerCase()
-
-   
     
-    if(command === 'ping') message.channel.send('pong!')
+    if(command === 'ping') 
+        message.channel.send('pong!')
 
-    if(command === 'roll') message.channel.send(rollDice())
+    if(command === 'roll') 
+        message.channel.send(rollDice())
 
-    if(command === 'quote') inspirationalQuote().then(quote => message.channel.send(quote.text + " -"+quote.author||"Anonymous"))
+    if(command === 'quote') 
+        inspirationalQuote().then(quote => message.channel.send(quote.text + " -"+quote.author||"Anonymous"))
     
-    if(command === 'cat') getCat().then(imageUrl => message.channel.send({files:[imageUrl]}))
+    if(command === 'cat') 
+        getCat().then(imageUrl => message.channel.send({files:[imageUrl]}))
 
+    if (command == "play")
+        client.distube.play(message.member.voice.channel, args.join(" "), {
+            member: message.member,
+            textChannel: message.channel,
+            message
+        })
+
+    if (command == "stop") {
+            client.distube.stop(message);
+            message.channel.send("Stopped the queue!");
+    }
 })
 client.login(auth.token)
+
+
+client.distube
+    .on('playSong', (queue, song) =>
+        queue.textChannel.send(`Now playing: \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`)
+    )
+
 
 function rollDice(){
     return (Math.floor(Math.random()*101)).toString()
