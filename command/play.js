b0f2fa77fa2b8db79f60abe8768058e155e22e5a
Paradox021 <espanacarmelo@gmail.com>
2022-11-11 00:26:29 +0100

    play command refactor

---
+++ command/play.js
@@ -0,0 +1,19 @@
+export const play = async (message, args, client) =>{
+    if (!message.member.voice.channel) {
+        message.channel.send(`${message.author} you must be in a voice channel to do this! `);
+        message.delete()
+        return;
+    }
+    try {
+        
+        await client.distube.play(message.member.voice.channel, args.join(" "), {
+            member: message.member,
+            textChannel: message.channel,
+            message
+        })
+        message.delete()
+
+    } catch (e) {
+        console.error(e.name, e.message)
+    }
+}
